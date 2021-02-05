dda535807df6139933698734baefb4fcfffda977
Redd56 <pr.redstone@gmail.com>
2021-02-04 20:12:26 -0800

    <removed some stuff added some stuff ya know the nomral>

--- src/main/java/net/redd/lawnage/GrassLawnBlock.java
+++ src/main/java/net/redd/lawnage/GrassLawnBlock.java
@@ -18,11 +18,13 @@ public class GrassLawnBlock extends Block implements Fertilizable{
 
     @Override
     public boolean isFertilizable(BlockView world, BlockPos pos, BlockState state, boolean isClient) {
+
         return true;
     }
 
     @Override
     public boolean canGrow(World world, Random random, BlockPos pos, BlockState state) {
+
         return true;
     }
 
