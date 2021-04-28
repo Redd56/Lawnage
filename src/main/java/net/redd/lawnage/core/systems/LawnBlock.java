9e749ce72689a375a599edad4bc1858d1db511a5
redd <pr.redstone@gmail.com>
2021-04-28 16:50:42 -0700

    removed redudancy

--- src/main/java/net/redd/lawnage/core/systems/LawnBlock.java
+++ src/main/java/net/redd/lawnage/core/systems/LawnBlock.java
@@ -1,6 +1,5 @@
 package net.redd.lawnage.core.systems;
 
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HorizontalFacingBlock;
@@ -16,6 +15,6 @@ public class LawnBlock extends HorizontalFacingBlock {
     }
 
     public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return (BlockState)this.getDefaultState().with(FACING, ctx.getPlayerFacing().getOpposite());
+        return this.getDefaultState().with(FACING, ctx.getPlayerFacing().getOpposite());
     }
 }
